0428237175bbeab40cb1b0956394c570c627dac0
cwtshh <gucosta1719@gmail.com>
2023-08-17 20:54:28 -0300

    adicionando testes

---
+++ Testes em Dart/dart_test/lib/dart_test.dart
@@ -0,0 +1,18 @@
+int calculate() {
+  return 6 * 7;
+}
+
+double calcularDesconto(double valor, double desconto, bool percentual) {
+  if (valor <= 0) {
+    throw ArgumentError("O valor do produto não pode ser 0");
+  }
+
+  if (desconto <= 0) {
+    throw ArgumentError("O valor do desconto não pode ser 0");
+  }
+
+  if (percentual) {
+    return valor - (valor * desconto / 100);
+  }
+  return valor - desconto;
+}
---
+++ Testes em Dart/dart_test/test/dart_test_test.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+
+import 'package:dart_test/dart_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+
+  test('Calcula o desconto do produto sem porcentagem', () {
+    expect(calcularDesconto(1000, 150, false), equals(850));
+  });
+
+  test('Calcula o desconto do produto com o preço zerado', () {
+    expect(() => calcularDesconto(0, 150, false),
+        throwsA(TypeMatcher<ArgumentError>()));
+  });
+
+  test('Calcula o desconto do produto com porcentagem', () {
+    expect(calcularDesconto(1000, 15, true), 850);
+  });
+
+  test('Calcula o desconto do produto com o preço zerado', () {
+    expect(() => calcularDesconto(1000, 0, true),
+        throwsA(TypeMatcher<ArgumentError>()));
+  });
+
+  group('Calcula o valor do produto com desconto', () {
+    var valuesToTest = {
+      {'valor': 1000, 'desconto': 150, 'percentual': false}: 850,
+      {'valor': 1000, 'desconto': 15, 'percentual': true}: 850,
+    };
+
+    valuesToTest.forEach((values, expected) {
+      test('Entrada: $values Esperado: $expected', () {
+        expect(
+            calcularDesconto(
+                double.parse(values['valor'].toString()),
+                double.parse(values['desconto'].toString()),
+                values['percentual'] == true),
+            equals(expected));
+      });
+    });
+  });
+}
